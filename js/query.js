1fe991b9179502587043781e8b071330030f76e4
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-04 16:12:38 -0400

    Added population of destination from first screen onto second screen

--- js/query.js
+++ js/query.js
@@ -1,18 +1,12 @@
-
-
-
-
-
-
-
-
-
-
 //------------------------------------
 map = instantiateMap();
 
 //Getting the destination from sessionStorage
-const destination = loadFromSession("destination").position;
-addMarker(destination, endIcon);
-map.setCenter(destination);
+const destination = loadFromSession("destination");
+addMarker(destination.position, endIcon);
+map.setCenter(destination.position);
 map.setZoom(17);
+
+//populating the text field with the destination
+// console.log(destination)
+$("#destination").text(destination.address.label);
