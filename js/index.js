f478e3706130a569b347e750faed78306267765f
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-04 15:22:11 -0400

    Removed spacing between navbar and map, added event listeners to search field

--- js/index.js
+++ js/index.js
@@ -5,25 +5,27 @@
  //Validates search input using geocode
  //If there are no options, proceed to next page saving the location
  // If there are multiple options, make modal where they choose, then go to next page
+     function submitSearch(){
+        console.log(`submitSearch called`)
+        //clear field and grab text
+        let searchText = $('#destination-search-field').val();
+        console.log("submitSearch -> searchText", searchText)
+        $('#destination-search-field').val('')
+        
+    }
+    
 
-instantiateMap()
-
-$('#destination-search-button').click(function(event){
-    event.preventDefault()
-    submitSearch()
-})
-
-$('#destination-search-field').on('keypress', function(event){
-    if (event.which === 13) {
+    instantiateMap()
+    
+    $('#destination-search-button').click(function(event){
         event.preventDefault()
         submitSearch()
-    }
-})
-
-function submitSearch(){
-    console.log(`submitSearch called`)
-    //clear field and grab text
-    let searchText = $('#destination-search-field').val();
-    console.log("submitSearch -> searchText", searchText)
-    $('#destination-search-field').val('')
-}
+    })
+    
+    $('#destination-search-field').on('keypress', function(event){
+        if (event.which === 13) {
+            event.preventDefault()
+            submitSearch()
+        }
+    })
+   
