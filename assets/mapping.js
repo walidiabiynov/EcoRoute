8f4f37de85a8a137e603a219dfafbd0bc391f07f
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-03 21:35:59 -0400

    Added functions to search between two points for each method of transit and provide distance, travel time, and the shape to be rendered on the map

--- assets/mapping.js
+++ assets/mapping.js
@@ -19,6 +19,16 @@ function instantiateMap() {
   return map;
 }
 
+function saveToSession(key, value){
+  //Just saves to sessionStorage, I'm outsourcing it to a function in case we change how we save data in the future
+  sessionStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromSession(key){
+  return JSON.parse(sessionStorage.getItem(key))
+}
+
+
 function addMarker(map, coords) {
   let svgMarkup =
     '<svg width="24" height="24" ' +
@@ -71,34 +81,75 @@ function addLines(map, arrayOfPoints) {
 }
 
 // WORKING ON ROUTING
-async function getRoute(startPoint, endPoint, method = "fastest;car") {
+async function getRoute(startPoint, endPoint, transitType) {
+  //This function accepts start and end objects with lat and lng, also an array that contains the name of the transit type ex. 'bicycle' and the key under which to store the data ex. distance-bike
+  let method = 'fastest;' + transitType[0]
+  let transitTypeKey = transitType[1]
+
   response = await fetch(
     `https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=${APIKey}&waypoint0=geo!${startPoint.lat},${startPoint.lng}&waypoint1=geo!${endPoint.lat},${endPoint.lng}&mode=${method};traffic:disabled&instructionformat=text&routeattributes=shape`
   );
-
+  
   if (response.ok) {
     response = await response.json();
-    map = instantiateMap();
-    console.log(response);
-    addLines(map, response.response.route[0].shape);
+    console.log(response)
+
+    let distanceTravelled = await response.response.route[0].summary.distance
+    let travelTime = await response.response.route[0].summary.travelTime
+    let transitText = await response.response.route[0].summary.text
+    let shape = await response.response.route[0].shape
+    console.log(`Running for ${transitTypeKey} - `, shape)
+    saveToSession(`distance-${transitTypeKey}`, distanceTravelled)
+    saveToSession(`traveltime-${transitTypeKey}`, travelTime)
+    saveToSession(`travel-text-${transitTypeKey}`, transitText)
+    saveToSession(`shape-${transitTypeKey}`, shape)
   }
+
+  return {route: response.response}
+  
+  
+  // map = instantiateMap();
+  // addLines(map, response.response.route[0].shape);
+
+
+
+
+
+
+  // //This function displays the map, renders the two points, and adds the lines for the route
+  // //  It also passes the
+  // response = await fetch(
+  //   `https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=${APIKey}&waypoint0=geo!${startPoint.lat},${startPoint.lng}&waypoint1=geo!${endPoint.lat},${endPoint.lng}&mode=${method};traffic:disabled&instructionformat=text&routeattributes=shape`
+  // );
+
+  // if (response.ok) {
+  //   response = await response.json();
+  //   map = instantiateMap();
+  //   console.log(response);
+  //   addLines(map, response.response.route[0].shape);
+  // }
+
 }
 
-function processSearch(searchString){
+function mapRoute(routeKey){
+  //This function takes the chosen route and maps it on the page
+  
+
+
+}
+
+
+function processSearch(searchString, destinationOrOrigin='destination'){
   //Takes a string of a location, ex. 8 Bloor St. W, Toronto
   //Returns all of the objects the string finds, to be chosen from
   // service.geocode({q: searchString}, populateChoiceList, function(error){console.log("Something went wrong", error)})
-  
-  let returnedLocationsArray = []
-  
+    
   let service = platform.getSearchService();
-  service.geocode({q: searchString}, populateChoiceList, function(error){console.log("Something went wrong", error)})
+  service.geocode({q: searchString}, function(returnedChoices){populateChoiceList(returnedChoices, destinationOrOrigin)}, function(error){console.log("Something went wrong", error)})
   
-  console.log("processSearch -> returnedLocationsArray", returnedLocationsArray)
-  return returnedLocationsArray
 }
   
-function populateChoiceList(returnedLocations){
+function populateChoiceList(returnedLocations, destinationOrOrigin){
   //This is a placeholder, ideally this populates our method of choosing (or chooses based on criteria we have) then calls the function to render it
   choices = returnedLocations.items
 
@@ -110,8 +161,7 @@ function populateChoiceList(returnedLocations){
     //We could either attach this function to a button or choose based on location
     choice = choices[0]
   }
-
-  placePinOnMap(map, choice)
+  saveToSession((destinationOrOrigin === 'destination') ? 'destination' : 'origin', choice)
 }
 
 
@@ -120,6 +170,25 @@ function placePinOnMap(map, locationObject){
   map.addObject(new H.map.Marker(locationObject.position))
   map.getViewModel().setLookAtData({position: locationObject.position, zoom: 16})
 }
+
+function getAllRoutes(){
+  let origin = loadFromSession('origin')
+  let destination = loadFromSession('destination')
+  let transitTypes = [['car', 'car'], ['bicycle', 'bike'], ['truck', 'truck'], ['pedestrian', 'walk'], ['publicTransport', 'pt']]
+
+  transitTypes.forEach(function(transitType){
+
+    getRoute(origin.position, destination.position, transitType)
+  })
+
+}
+
+//TESTING AREA for getting all routes between two points
+// sessionStorage.clear()
+// processSearch('8 Bloor St. W, Toronto')
+// processSearch('499 Church St. Toronto', 'origin')
+// setTimeout(getAllRoutes, 2000)
+
 
 // Pardon this, this is here while I work out all of the options to request different data from the api
 // https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=x5fo9v5HB4bh1Boxq98XUddpp4vr2x9NRelXAaPyt0E&waypoint0=geo!43.687850,-79.395514&waypoint1=geo!43.664739,-79.413175&mode=fastest;car;traffic:disabled&instructionformat=text
@@ -136,4 +205,4 @@ function placePinOnMap(map, locationObject){
 //SITUATION: On main page, destination typed into search bar and search is pressed
 // let tempPlaceholderName = '8 Bloor St. W, Toronto'
 // map = instantiateMap();
-// returnedLocationsArray = processSearch(tempPlaceholderName)
+// processSearch(tempPlaceholderName)
