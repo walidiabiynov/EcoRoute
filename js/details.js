8606b1c04821ed4543a472164b48ad326b73e4c0
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-06-05 18:55:42 -0400

    Made graph populate with all searched co2 values

--- js/details.js
+++ js/details.js
@@ -1,9 +1,13 @@
-// Getting co2 values from session storage
-
 // Get results array from local storage
+var storageResults = JSON.parse(sessionStorage.getItem("results"));
+var allModes = [];
+var co2Values = [];
+
 // Split up results array into two new arrays to populate the graph
-    // co2 values only
-    // transport modes only
+for(i=0; i<storageResults.length; i++){
+    allModes.push(storageResults[i].mode);
+    co2Values.push(storageResults[i].co2);
+}
 
 // Create 3 comparison values for context of co2 impact
     // 1 tree can remove 48 lbs of co2 in a year https://www.co2meter.com/blogs/news/could-global-co2-levels-be-reduced-by-planting-trees#:~:text=A%20typical%20hardwood%20tree%20can,it%20reaches%2040%20years%20old.
