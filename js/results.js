2d4bb1d8e23dbfac6c789f5bbf4840f775eb7b3d
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-05 16:32:00 -0400

    Added creation of cards on results page

--- js/results.js
+++ js/results.js
@@ -1,13 +1,22 @@
-
-
-
-
-
-
-
-
-
-
 //-----------------------------------------
-destination = loadFromSession('destination')
-$('#destination-feedback').text(destination.address.label)
+destination = loadFromSession("destination");
+$("#destination-feedback").text(destination.address.label);
+
+const chosenTransportMethods = loadFromSession("results")
+const rowEl = $('#results-row')
+
+chosenTransportMethods.forEach(function (transportMethod, index) {
+
+  let cardElText =
+    `<div class="col-sm-3"><div class="card"><div class="card-body">` +
+    `<h5 class="card-title">${
+      transportNameVals[transportNameKeys.indexOf(transportMethod.mode)]
+    }</h5>` +
+    `<div class="card-text">` +
+    `<p>CO2: ${transportMethod.co2.toFixed(1)}</p>` +
+    `<p>Time: ${(loadFromSession(`traveltime-${transportMethod.mode}`)/60).toFixed(1)} minutes</p>` +
+    `<p>Distance: ${loadFromSession(`distance-${transportMethod.mode}`).toFixed(1)} km</p>` +
+    `<button class="btn btn-success">Learn More</button>` +
+    `</div></div></div></div>`;
+    rowEl.append(cardElText)
+});
