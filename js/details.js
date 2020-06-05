fd50f3da569aed02d78a920dbdfd6d216c703040
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-05 19:16:03 -0400

    Added route being mapped to the final page

--- js/details.js
+++ js/details.js
@@ -18,4 +18,4 @@ const chosenMethod = loadFromSession('detail-method-selected')
 if (! chosenMethod) {
     errorModal("You haven't selected a method to see details for, please return to the previous page")
 }
-mapRoute(chosenTransportMethods[0].mode)
+mapRoute(mapKeyTranslator(chosenMethod))
