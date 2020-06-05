8b5882d965c22931b0cb28470b6aa81c71bb9f97
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-06-05 18:21:22 -0400

    Pseudocoded details.js

--- js/details.js
+++ js/details.js
@@ -1,1 +1,11 @@
 // Getting co2 values from session storage
+
+// Get results array from local storage
+// Split up results array into two new arrays to populate the graph
+    // co2 values only
+    // transport modes only
+
+// Create 3 comparison values for context of co2 impact
+    // 1 tree can remove 48 lbs of co2 in a year https://www.co2meter.com/blogs/news/could-global-co2-levels-be-reduced-by-planting-trees#:~:text=A%20typical%20hardwood%20tree%20can,it%20reaches%2040%20years%20old.
+    // Find no. 2
+    // Find no. 3 
