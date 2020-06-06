d4263d8a1027e568e654ecc35e92f437c81eef54
titanian229 <48775473+titanian229@users.noreply.github.com>
2020-06-05 22:43:51 -0400

    changed location at which route is calculated

--- js/query.js
+++ js/query.js
@@ -21,6 +21,9 @@ async function submitSearch() {
   }
   saveToSession("origin", choice);
   $('#origin').text(choice.address.label.split(',').slice(0, 2).toString())
+  if (loadFromSession("destination") && loadFromSession("origin")) {
+    await getAllRoutes()
+  }
   //Location is saved, we can move to next page
   // window.location = "./results.html";
 
@@ -32,8 +35,7 @@ async function getDirectionsButton() {
   //This needs to be in an async function so I can await, otherwise the page changes and it doesn't actually do the math
 
   //a check they've input both locations
-  if (loadFromSession("destination") && loadFromSession("origin")) {
-    await getAllRoutes();
+
     // Check if at lest one transport option has been selected
     // Save resultsList to sessionStorage
     if(resultsList.length > 0){
@@ -42,12 +44,12 @@ async function getDirectionsButton() {
     } else {
         errorModal('Please choose at least one mode of transportation to proceed.');
     }
-  }
+  
 }
 
-function getLocation() {
+async function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(async function (position) {
       //TODO put in the call to the generalized function for setting the place name
       position = {
         lat: position.coords.latitude,
@@ -55,6 +57,9 @@ function getLocation() {
       };
       $("#origin").text(`${position.lat}, ${position.lng}`); //TODO Reverse geocode and get location name, fill that here
       saveToSession("origin", { position: position });
+      if (loadFromSession("destination") && loadFromSession("origin")) {
+        await getAllRoutes()
+      }
     });
   } else {
     console.log("Geolocation unsupported by this browser");
@@ -101,3 +106,47 @@ $("#geolocation-search").click(function (event) {
 //TODO catch error values
 //TODO catch direct loads of this page
 //TODO catch if there is no destination value from the previous page
+
+
+
+///THIS IS JUST A TEST
+
+// const options = [
+//   { 
+//       // CO2 emission in g/km, data by: https://www.carbonfootprint.com/ 
+//       id: "car",
+//       mode: ["micro-car", "compact-car", "sedan", "suv"],
+//       gas: ["gasoline", "diesel", "electric"],
+//       coEmissionGasoline: [ 125.0692, 147.144, 156.4524, 230.2278 ],
+//       coEmissionDiesel: [ 115.6062, 138.82, 141.0578, 170.4394 ],
+//       coEmissionElectric: 7.7, // Considering the current mix of energy production in Ontario: https://www.cer-rec.gc.ca/nrg/ntgrtd/mrkt/snpsht/2018/09-01-1hwrnrgprjctsfnncd-eng.html
+//   }, 
+//   {
+//       // Generalized CO2 emission for trucks: https://www.transportenvironment.org/sites/te/files/publications/2015%2009%20TE%20Briefing%20Truck%20CO2%20Too%20big%20to%20ignore_FINAL.pdf
+//       id: "truck",
+//       mode: "truck",        
+//       coEmission: 924
+//   },
+//   { 
+//       // CO2 emission in g/km, TTC as an example: http://www.ttc.ca/PDF/About_the_TTC/Sustainability_Reports/2013_Sustainability_Report.PDF
+//       id: "pt",
+//       mode:"publicTransport",
+//       coEmission: 64
+//   }, 
+//   {
+//       id: "bike",
+//       mode: "bike",
+//       coEmission: 0
+//   }, 
+//   {
+//       id: "walk",
+//       mode: "pedestrian",
+//       coEmission: 0
+//   }
+// ]
+
+// $("#choice-buttons").click(function(event){
+//   if (event.target.matches('button')){
+//     event.target.toggleClass('active')
+//   }
+// })
