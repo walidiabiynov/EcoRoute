7f63088dd264087283e7b7409017051f71dfb1a7
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-06-27 17:04:38 -0400

    Weather advisory working now

--- js/weather.js
+++ js/weather.js
@@ -31,21 +31,35 @@ function determineOptions(){
     return {walk: walkStatus, bike: bikeStatus}
 }
 
+// Creating options for picking the weather advice from
+function createMethodString(prefix, optionText, options) {
+    const methodStrings = [];
+    if (options.walk) methodStrings.push(optionText.walk);
+    if (options.bike) methodStrings.push(optionText.bike);
+
+    if (methodStrings.length) {
+        return `${prefix} ${methodStrings.join(' or ')}!`;
+    }
+
+    return '';
+}
+
 // Render weather
 function renderWeather(){
     // Determine conditions
+    var options = determineOptions();
     var currentConditionId = weather.current.weather[0].id;
     var currentDescription = weather.current.weather[0].description;
     if(currentConditionId >= 800){
-        document.getElementById("condition").textContent = "It looks dry along your route. You might want to consider walking or taking the bike!"
+        document.getElementById("condition").textContent = `It looks dry along your route. ${createMethodString('You might want to consider', { walk: 'walking', bike: 'taking the bike'}, options)}`
     } else if(currentConditionId >= 700 && currentConditionId < 800){
-        document.getElementById("condition").textContent = `Looks like we have some ${currentDescription} on your route. Walking or taking the bike might not be the best idea.`
+        document.getElementById("condition").textContent = `Looks like we have some ${currentDescription} on your route. ${createMethodString('It might not be the best idea', { walk: 'to walk', bike: 'to take the bike'}, options)}`
     } else if(currentConditionId >= 600 && currentConditionId < 700){
-        document.getElementById("condition").textContent = `It's cold and there's ${currentDescription} on your route. Take care when walking or taking the bike!`
+        document.getElementById("condition").textContent = `It's cold and there's ${currentDescription} on your route. ${createMethodString('Take care when', { walk: 'walking', bike: 'taking the bike'}, options)}`
     } else if(currentConditionId >= 300 && currentConditionId < 600){
-        document.getElementById("condition").textContent = `It's wet and there's ${currentDescription} on your route. Walking or taking the bike might not be the best idea.`
+        document.getElementById("condition").textContent = `It's wet and there's ${currentDescription} on your route. ${createMethodString('Take care when', { walk: 'walking', bike: 'taking the bike'}, options)}`
     } else if(currentConditionId >= 200 && currentConditionId < 300){
-        document.getElementById("condition").textContent = `Oh no, there's a ${currentDescription}. Walking or taking the bike might be very unsafe.`
+        document.getElementById("condition").textContent = `Oh no, there's a ${currentDescription}. ${createMethodString('It might be very unsafe', { walk: 'to walk', bike: 'to take the bike'}, options)}`
     } 
     document.getElementById("temperature").textContent = `It's ${(weather.current.temp-273.15).toFixed(1)} °C.`;
     document.getElementById("weather-icon").innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png"  alt="Icon today's weather"/>`
