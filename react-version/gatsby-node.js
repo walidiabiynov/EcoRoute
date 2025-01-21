bfa26300b358f1bbc5b8a4a01e3b0176427b042f
WaliThiga <166392016+walidiabiynov@users.noreply.github.com>
2025-01-21 15:26:53 +0100

    Initial commit

--- react-version/gatsby-node.js
+++ react-version/gatsby-node.js
@@ -1,7 +1,15 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+      resolve: {
+        fallback: {
+          crypto: require.resolve('crypto-browserify'),
+        },
+      },
+      plugins: [
+        new (require('webpack')).DefinePlugin({
+          'process.env.NODE_OPTIONS': JSON.stringify('--openssl-legacy-provider'),
+        }),
+      ],
+    });
+  };
+  
